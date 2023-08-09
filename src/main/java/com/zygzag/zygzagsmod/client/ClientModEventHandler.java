f0b244330ad493fc5cf6bbb5978ac35addc1b7b7
ZygZagGaming <46930418+ZygZagGaming@users.noreply.github.com>
2023-08-09 16:00:18 -0400

    Update forge version + fix bug
    Updated forge version from 47.1.25 to 47.1.65
    Fixed bug that prevented dedicated server from starting

--- src/main/java/com/zygzag/zygzagsmod/client/ClientModEventHandler.java
+++ src/main/java/com/zygzag/zygzagsmod/client/ClientModEventHandler.java
@@ -5,6 +5,7 @@ import com.zygzag.zygzagsmod.common.registries.EntityRegistry;
 import com.zygzag.zygzagsmod.common.registries.ParticleTypeRegistry;
 import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraft.client.renderer.entity.WitherSkeletonRenderer;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,7 +13,7 @@ import net.minecraftforge.fml.common.Mod;
 
 import static com.zygzag.zygzagsmod.common.Main.MODID;
 
-@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientModEventHandler {
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
