0e040582595e5469035e5fdf953ef173e22d2964
BLAZED-Shard <akokane3369@gmail.com>
2024-01-12 21:01:40 -0500

    Removed visual issues and hardcoded some  actionlengths

--- src/main/java/com/zygzag/zygzagsmod/common/registry/ActionRegistry.java
+++ src/main/java/com/zygzag/zygzagsmod/common/registry/ActionRegistry.java
@@ -38,9 +38,9 @@ public class ActionRegistry extends CustomAkomiRegistry<Action> {
 
     public static class BlazeSentry {
         public static final Supplier<Action> IDLE_BASE = INSTANCE.register("blaze_sentry_idle_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.idle_loop"), 60, true));
-        public static final Supplier<Action> SHOOT_BASE = INSTANCE.register("blaze_sentry_shoot_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.shooting_loop"), 60, true));
-        public static final Supplier<Action> AGRO_BASE = INSTANCE.register("blaze_sentry_agro_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.agro_loop_2"), 40, true));
-        public static final Supplier<Action> SHOOT_BIG_BASE = INSTANCE.register("blaze_sentry_shoot_big_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.shoot_big_fireball"), 40, false));
+        public static final Supplier<Action> SHOOT_BASE = INSTANCE.register("blaze_sentry_shoot_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.shooting_loop"), 60, false));
+        public static final Supplier<Action> AGRO_BASE = INSTANCE.register("blaze_sentry_agro_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.agro_loop_2"), 40, false));
+        public static final Supplier<Action> SHOOT_BIG_BASE = INSTANCE.register("blaze_sentry_shoot_big_base", () -> new Action(RawAnimation.begin().thenPlay("animation.blaze_sentry.shoot_big_fireball"), 53, false));
 
         public static void init() {
         }
